7810786c0f5610c3935d6c98216011e6ae25707b
Pavadharani25 <110721613+Pavadharani25@users.noreply.github.com>
2022-11-01 19:38:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let display = document.getElementById('result');
+let input = 0;
+
+function type(el) {
+    if (input == 0) {
+        input = el.innerText;
+    } else {
+        input += el.innerText;
+    }
+
+    display.innerText = input;
+}
+
+document.getElementById("clear").addEventListener('click', function () {
+    display.innerText = 0;
+    input = 0;
+});
+
+document.getElementById("equal").addEventListener('click', function () {
+    let result = 0;
+    result = eval(display.innerText);
+
+        if (isNaN(result) || result == 'Infinity')
+  {
+        display.innerText = 'Math Error';
+    } else {
+        result = (result % 1 === 0) ? result : result.toFixed(15);
+        display.innerText = result;
+    }
+
+
+
+    input = result;
+});
+
+
+
+document.getElementById("delete").addEventListener('click', function () {
+    display = document.getElementById('result');
+    result = display.innerText.slice(0, -1);
+    display.innerText = result;
+    input = result;
+});
